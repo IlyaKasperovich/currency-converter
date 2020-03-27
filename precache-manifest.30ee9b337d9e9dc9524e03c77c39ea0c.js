69afd5132c3d1a26865fb640614ecfbe8a1432e1
Ilya Kasperovich <kasper.ii2108@gmail.com>
2020-03-27 14:45:59 +0300

    Updates

---
+++ precache-manifest.30ee9b337d9e9dc9524e03c77c39ea0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d05e1ec2b504763ec8a5734d959346a",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "75897a4401582985ed52",
+    "url": "/currency-converter/static/css/main.fd3e6ad4.chunk.css"
+  },
+  {
+    "revision": "6365ab69b132875a32d1",
+    "url": "/currency-converter/static/js/2.8a61f7b7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/currency-converter/static/js/2.8a61f7b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75897a4401582985ed52",
+    "url": "/currency-converter/static/js/main.5de5ed58.chunk.js"
+  },
+  {
+    "revision": "bbf76792afd43ba1a3e0",
+    "url": "/currency-converter/static/js/runtime-main.01bd5dc3.js"
+  }
+]);
